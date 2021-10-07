8d68f20fd1b84bb5a9d0034693e9cb2cd2935370
ganaga98 <73050866+ganaga98@users.noreply.github.com>
2021-10-07 05:49:51 +0530

    Display Toast Message through html

---
+++ app.js
@@ -0,0 +1,16 @@
+$.toast({
+    heading: 'Happy Morning !!!',
+    text: 'Ganaga',
+    position: 'top-center',
+    icon: 'info'
+})
+
+
+$.toast({
+    heading: 'Happy Morning !!!',
+    text: 'Ganaga',
+    position: 'top-center',
+    icon: 'success',
+    hideAfter: false
+})
+
